a34884814d61df06f5d0acaa43340432fb158cef
Alexanderrrr <acons1987@gmail.com>
2018-12-07 18:33:15 +0100

    Zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <nav-bar />
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavBar
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import TriviaApp from './components/TriviaApp'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  {path:'/', component: TriviaApp},
+
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
